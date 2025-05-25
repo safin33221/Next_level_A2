3f38bf9c7c7c99c408654fa781d59a03c4c70c4e
safin33221 <safin33221@gmail.com>
2025-05-25 23:53:42 +0600

    Add species table with initial structure

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -9,6 +9,15 @@ SELECT * FROM rangers;
 
 
 
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status  VARCHAR(50) DEFAULT 'Unknown'
+);
+
+
 
 
 
