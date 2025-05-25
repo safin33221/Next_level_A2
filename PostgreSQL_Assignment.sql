67e798e9a506b2048590c8dfe300db208639e5b3
safin33221 <safin33221@gmail.com>
2025-05-25 23:50:48 +0600

    Add rangers table with initial structure

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,11 @@
 -- Active: 1748191021305@@127.0.0.1@5432@conservation_db@public
+CREATE TABLE rangers (
+    rangers_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+SELECT * FROM rangers;
 
 
 
