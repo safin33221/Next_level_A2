35403c7148b291438518bcb7a99db7fa99dca093
safin33221 <safin33221@gmail.com>
2025-05-26 17:00:49 +0600

    Problem 6 : Completed to Show the most recent 2 sightings.

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -98,4 +98,12 @@ left JOIN sightings as si ON sp.species_id = si.species_id
 WHERE si.sighting_id IS NULL;
 
 
+-- Problem 6 : Show the most recent 2 sightings.
+
+SELECT sp.common_name,sighting_time ,r.name FROM sightings as si
+    JOIN species as sp ON si.species_id = sp.species_id
+    JOIN rangers as r  ON si.ranger_id = r.ranger_id
+    ORDER BY si.sighting_time DESC
+    LIMIT 2;   
+
 
